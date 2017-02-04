ad286f64aa1a5c08dd5e218c33079ef9abb23f58
kephin <wunshon76325@gmail.com>
2017-02-04 23:55:36 +0800

    replace with TDD style

--- 412_FizzBuzz.js
+++ 412_FizzBuzz.js
@@ -1,35 +1,38 @@
-###Fizzbuzz
-**Description:**
+/*
+412. Fizz Buzz
 
 Write a program that outputs the string representation of numbers from 1 to n.
-
 But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. For numbers which are multiples of both three and five output “FizzBuzz”.
+*/
 
-**Example:**
+const expect = require('expect');
 
-n = 15,
-Return:
-[
-  "1",
-  "2",
-  "Fizz",
-  "4",
-  "Buzz",
-  "Fizz",
-  "7",
-  "8",
-  "Fizz",
-  "Buzz",
-  "11",
-  "Fizz",
-  "13",
-  "14",
-  "FizzBuzz"
-]
+it('412 Fizz Buzz', () => {
+  //arrange
+  const n = 15;
+  const expected = [
+    '1',
+    '2',
+    'Fizz',
+    '4',
+    'Buzz',
+    'Fizz',
+    '7',
+    '8',
+    'Fizz',
+    'Buzz',
+    '11',
+    'Fizz',
+    '13',
+    '14',
+    'FizzBuzz',
+  ];
+  //act
+  const target = fizzBuzz(n);
+  //assert
+  expect(target).toEqual(expected);
+});
 
-**Solution:**
-
-```javascript
 const fizzBuzz = (n) => {
   const arr = [];
   for (let index = 1; index <= n; index++) {
@@ -45,4 +48,3 @@ const fizzBuzz = (n) => {
   }
   return arr;
 };
-```
