1636bb9a6110c0ed3a7a1552b24ee5ca4b57be22
kephin <wunshon76325@gmail.com>
2017-02-06 23:17:24 +0800

    fix number overflow issue and add init value of reduce

--- 461_Hamming_Distance.js
+++ 461_Hamming_Distance.js
@@ -10,22 +10,66 @@ Note:
 
 const expect = require('expect');
 
-it('461 Hamming Distance', () => {
-  //arrange
-  const x = 1;
-  const y = 4;
-  const expected = 2;
-  //act
-  const actual = hammingDistance(x, y);
-  //assert
-  expect(actual).toBe(expected);
+describe('461 Hamming Distance', () => {
+  it('regular situation', () => {
+    //arrange
+    const x = 1;
+    const y = 4;
+    const expected = 2;
+    //act
+    const actual = hammingDistance(x, y);
+    //assert
+    expect(actual).toBe(expected);
+  });
+
+  it('large number', () => {
+    //arrange
+    const x = 1577962638;
+    const y = 1727613287;
+    const expected = 16;
+    //act
+    const actual = hammingDistance(x, y);
+    //assert
+    expect(actual).toBe(expected);
+  });
+
+  it('same number', () => {
+    //arrange
+    const x = 16;
+    const y = 16;
+    const expected = 0;
+    //act
+    const actual = hammingDistance(x, y);
+    //assert
+    expect(actual).toBe(expected);
+  });
 });
 
 const hammingDistance = (x, y) => {
-  return (parseInt(x.toString(2)) + parseInt(y.toString(2)))
-    .toString()
+  if (x === y) return 0;
+
+  // make sure x is larger
+  if (x < y) {
+    [x, y] = [y, x];
+  }
+
+  const [parseLarger, parseLesser] = [x, y].map(item => item
+    .toString(2)
     .split('')
-    .map(value => parseInt(value))
+    .map(value => parseInt(value)));
+
+  const lengthDiff = parseLarger.length - parseLesser.length;
+  const newArray = [];
+
+  for (let index = 0; index < parseLarger.length; index++) {
+    if (index < lengthDiff) {
+      newArray.push(parseLarger[index]);
+    } else {
+      newArray.push(parseLarger[index] + parseLesser[index - lengthDiff]);
+    }
+  }
+
+  return newArray
     .filter(value => value === 1)
-    .reduce((prev, next) => prev + next);
+    .reduce((prev, next) => prev + next, 0);
 };
