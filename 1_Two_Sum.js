ce4f716bad028e2a9c32ded50a751829eacbcd7f
kephin <wunshon76325@gmail.com>
2017-05-16 23:42:29 +0800

    1_two_sum

---
+++ 1_Two_Sum.js
@@ -0,0 +1,31 @@
+/*
+1. Two Sum
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution.
+*/
+
+const expect = require('expect');
+
+describe('1 Two Sum', () => {
+  it('returns true', () => {
+    //arrange
+    const nums = [2, 7, 11, 15];
+    const target = 9;
+    const expected = [0, 1];
+    //act
+    const actual = twoSum(nums, target);
+    //assert
+    expect(actual).toEqual(expected);
+  });
+});
+
+// O(n^2)
+const twoSum = (nums, target) => {
+  for (let indexFirst = 0; indexFirst < nums.length; indexFirst++) {
+    for (let indexSecond = indexFirst + 1; indexSecond < nums.length; indexSecond++) {
+      if (nums[indexFirst] + nums[indexSecond] === target) return [indexFirst, indexSecond];
+    }
+  }
+};
+
