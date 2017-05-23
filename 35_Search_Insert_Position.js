9087022dac3d8c7860041147b2e2e288540c255f
kephin <wunshon76325@gmail.com>
2017-05-23 23:40:20 +0800

    35_search_insert_position

---
+++ 35_Search_Insert_Position.js
@@ -0,0 +1,62 @@
+/*
+35. Search Insert Position
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+*/
+
+const expect = require('expect');
+
+describe('35 Search Insert Position', () => {
+  it('returns inserted position should be 2', () => {
+    //arrange
+    const nums = [1, 3, 5, 6];
+    const target = 5;
+    const expected = 2;
+    //act
+    const actual = searchInsert(nums, target);
+    //assert
+    expect(actual).toBe(expected);
+  });
+  it('returns inserted position should be 1', () => {
+    //arrange
+    const nums = [1, 3, 5, 6];
+    const target = 2;
+    const expected = 1;
+    //act
+    const actual = searchInsert(nums, target);
+    //assert
+    expect(actual).toBe(expected);
+  });
+  it('returns inserted position should be 0', () => {
+    //arrange
+    const nums = [1, 3, 5, 6];
+    const target = 0;
+    const expected = 0;
+    //act
+    const actual = searchInsert(nums, target);
+    //assert
+    expect(actual).toBe(expected);
+  });
+  it('returns inserted position should be 4', () => {
+    //arrange
+    const nums = [1, 3, 5, 6];
+    const target = 7;
+    const expected = 4;
+    //act
+    const actual = searchInsert(nums, target);
+    //assert
+    expect(actual).toBe(expected);
+  });
+});
+
+// O(n) time
+const searchInsert = (nums, target) => {
+  if (target < nums[0]) return 0;
+  if (target > nums[nums.length - 1]) return nums.length;
+  for (let index = 0; index < nums.length; index++) {
+    if (nums[index] === target) return index;
+    if (target > nums[index] && target < nums[index + 1]) return index + 1;
+  }
+};
